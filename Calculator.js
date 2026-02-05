11c8321ecaa24490803313bb487ab9cc874e0197
Duh?210099 <miguelsaldanha1213@gmail.com>
2026-02-05 18:45:44 -0300

    Implement basic calculator functions in Calculator.js

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('display');
+
+function add(valor) {
+    display.value += valor;
+}
+
+
+function limpar() {
+    display.value = "";
+}
+
+function apagar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calcular() {
+    try {
+        
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+        setTimeout(limpar, 1500);
+    }
+}
